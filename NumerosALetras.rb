f0ef50f40f7781c061146eb1c9993b240ceb3af9
Jose Francisco Perez <josefcoperez@yahoo.es>
2017-05-31 23:12:20 +0200

    Primer commit. El número es correcto

---
+++ NumerosALetras.rb
@@ -0,0 +1,48 @@
+class Ordinal
+
+	def initialize(numero)
+	
+		end
+end
+
+def entrada_numero()
+	opcion = "S"	
+	while opcion == "S"
+		borrar_pantalla()
+		print "¿Quieres convertir un número a letras? (S/N): "
+		opcion = gets.chomp.upcase
+		if opcion == "S"
+			puts "Introduce un número positivo y con dos decimales como máximo: "
+			numero = gets.chomp
+			comprobar_numero(numero)
+			ordinal = Ordinal.new(numero)
+		end
+	end
+end
+
+def borrar_pantalla
+		system('clear')
+end
+
+def comprobar_numero(numero)
+	numero_a_comprobar = numero
+	numero_ok = false
+	while numero_ok == false
+		numero_comprobado = es_numero(numero_a_comprobar)
+		numero_ok = true
+	end
+end
+
+
+def es_numero(numero_a_comprobar)
+	while !(Float(numero_a_comprobar) rescue false) || numero_a_comprobar.to_f < 0
+		puts "No has introducido un número correcto. Pulsa Intro"
+		gets.chomp
+		borrar_pantalla
+		puts "Introduce un número positivo y con dos decimales como máximo: "
+		numero_a_comprobar = gets.chomp
+	end
+	numero_a_comprobar = sprintf("%.2f",numero_a_comprobar)
+end
+
+entrada_numero()
